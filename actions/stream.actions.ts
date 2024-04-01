d82be496745c50b47e11fe701c5b3b8cfac1dda7
washua2 <80311147+washua2@users.noreply.github.com>
2024-04-01 16:29:49 +0800

    Add files via upload

---
+++ actions/stream.actions.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { currentUser } from '@clerk/nextjs/server';
+import { StreamClient } from '@stream-io/node-sdk';
+
+const STREAM_API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const STREAM_API_SECRET = process.env.STREAM_SECRET_KEY;
+// const STREAM_API_KEY = "pk_test_Y2FyZWZ1bC1zdGlua2J1Zy01OC5jbGVyay5hY2NvdW50cy5kZXYk";
+// const STREAM_API_SECRET = "sk_test_Tz0FISYlPgGStic08G4eiVcaOtBYpcph8Gizz6dX7T";
+
+export const tokenProvider = async () => {
+  const user = await currentUser();
+
+  if (!user) throw new Error('User is not authenticated');
+  if (!STREAM_API_KEY) throw new Error('Stream API key secret is missing');
+  if (!STREAM_API_SECRET) throw new Error('Stream API secret is missing');
+
+  const streamClient = new StreamClient(STREAM_API_KEY, STREAM_API_SECRET);
+
+  const expirationTime = Math.floor(Date.now() / 1000) + 3600;
+  const issuedAt = Math.floor(Date.now() / 1000) - 60;
+
+  const token = streamClient.createToken(user.id, expirationTime, issuedAt);
+
+  return token;
+};
